ade4c8bb2aa4d90794b6f69d21022546afd04fca
AlysonRTY <variationsforpiano@gmail.com>
2025-04-09 15:56:12 +0200

    good commit

--- server/server.ts
+++ server/server.ts
@@ -2,8 +2,18 @@ import express from "express";
 import "dotenv/config";
 import dotenv from "dotenv";
 import cors from "cors";
+import router from "./routes/ducks.ts"; //need o be fixed
+import mongoose from "mongoose";
 
 dotenv.config();
+
+
+mongoose
+  .connect(process.env.MONGO_URI || "mongodb") //need to change tmr continue epic 8
+  .then(() => console.log("Connection to Mongo DB established"))
+  .catch((err) => console.log(err));
+
+
 const app = express();
 
 app.use(express.json());
@@ -13,6 +23,7 @@ app.use(
   })
 );
 app.use(cors());
+app.use("/ducks", router);
 
 
 
@@ -20,7 +31,7 @@ app.use(cors());
 app.get("/", (req, res) => {
     res.send("server is running");
 });
-const port = process.env.PORT ?? 8000
+const port = process.env.PORT || 8000
 
 app.listen(port, () => {
   console.log("Server is running on http://localhost:"+port);
