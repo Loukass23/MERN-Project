465d2f1d63a1568b047bf0d99e9766cbbaef4b22
AlysonRTY <variationsforpiano@gmail.com>
2025-04-22 15:36:28 +0100

    okay commit

--- client/src/pages/Ducks.tsx
+++ client/src/pages/Ducks.tsx
@@ -22,9 +22,9 @@ function Ducks() {
 
   return (
     <>
-      <div className="min-h-screen bg-gradient-to-b from-blue-50 to-white py-8 px-4 sm:px-6 lg:px-8">
+      <div className="mt-12 min-h-screen bg-gradient-to-b from-blue-50 to-white py-8 px-4 sm:px-6 lg:px-8">
         <div className="max-w-7xl mx-auto">
-          <h1 className="text-4xl font-bold text-center text-blue-600 mb-12">
+          <h1 className="text-4xl font-bold text-center text-yellow-400 mb-12">
             Hello Duck Friends!
           </h1>
 
