6ab1f24c46bf07b976b5a59447db8c8a24b19fd1
Lucas Dupias <lucas@codeacademyberlin.com>
2025-05-27 15:17:59 +0200

    rm fs function

--- server/dist/middleware/uploadMiddleware.js
+++ server/dist/middleware/uploadMiddleware.js
@@ -5,12 +5,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const multer_1 = __importDefault(require("multer"));
 const path_1 = __importDefault(require("path"));
-const fs_1 = __importDefault(require("fs"));
 // uploads directory exists
-const uploadDir = path_1.default.join(__dirname, "../../tmp");
-if (!fs_1.default.existsSync(uploadDir)) {
-    fs_1.default.mkdirSync(uploadDir, { recursive: true });
-}
+const uploadDir = path_1.default.join("/tmp");
+// if (!fs.existsSync(uploadDir)) {
+//   fs.mkdirSync(uploadDir, { recursive: true });
+// }
 const storage = multer_1.default.diskStorage({
     destination: (req, file, cb) => {
         cb(null, uploadDir);
--- server/middleware/uploadMiddleware.ts
+++ server/middleware/uploadMiddleware.ts
@@ -1,12 +1,12 @@
 import multer from "multer";
 import path from "path";
-import fs from "fs";
 
 // uploads directory exists
-const uploadDir = path.join(__dirname, "../../tmp");
-if (!fs.existsSync(uploadDir)) {
-  fs.mkdirSync(uploadDir, { recursive: true });
-}
+const uploadDir = path.join("/tmp");
+
+// if (!fs.existsSync(uploadDir)) {
+//   fs.mkdirSync(uploadDir, { recursive: true });
+// }
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
